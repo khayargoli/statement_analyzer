af9fd7dc6f44640c504fe2c1b5c66cee833b6a4a
Biswas Khayargoli <biswas.khayargoli@gmail.com>
2023-04-11 16:26:27 +0545

    Stable

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import tabula as tb
-import seaborn as sns
 import math
 pd.pandas.set_option("display.max_rows", None)
 pd.pandas.set_option("display.max_columns", None)
