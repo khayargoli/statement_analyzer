caac3cd591d81130ca25a34957ed686d8f3ed555
Biswas Khayargoli <developer@skred.me>
2024-02-17 23:05:58 +0545

    new pdf

--- main.py
+++ main.py
@@ -9,15 +9,18 @@ pd.pandas.set_option("display.max_columns", None)
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    dfs = tb.read_pdf(uploaded_file, pages='all', multiple_tables = True, columns=[27.0,68.0,272.0,357.5,397.0,474.5,553.0,631.0])
+    dfs = tb.read_pdf(uploaded_file, pages='all',  multiple_tables = True,  area=[185, 420, 1130, 800], columns=[420, 500,  590, 660, 730])
     df = pd.concat(dfs)
-    df = df.dropna()
+    st.write(df.head())
+    df = df.drop(['Unnamed: 0', 'Cheque Number'], axis=1)
 
+    #df = df.dropna()
+    df = df[df['Value Date'] != 'null']
+    
     st.title('Statement Analysis')
     st.markdown("Record size:")
-    st.write(df)
-
-    st.markdown("Anaylyzing statement from: " + df.head(1)['Transaction Date'].values[0].split(' ')[0] + " to " + df.tail(1)['Transaction Date'].values[0].split(' ')[0])
+    st.write(df.shape)
+    st.markdown("Anaylyzing statement from: " + df.head(1)['Value Date'].values[0].split(' ')[0] + " to " + df.tail(1)['Value Date'].values[0].split(' ')[0])
     
     df['Withdraw'] = df["Withdraw"].replace('-','0',inplace=False)
     df['Withdraw'] = df["Withdraw"].replace(',','',inplace=False, regex=True)
@@ -25,37 +28,38 @@ if uploaded_file is not None:
     df['Deposit'] = df["Deposit"].replace(',','',inplace=False, regex=True)
     df['Withdraw'] = pd.to_numeric(df['Withdraw'])
     df['Deposit'] = pd.to_numeric(df['Deposit'])
-    df['Transaction Date'] = pd.to_datetime(df['Transaction Date'])
+    df['Value Date'] = pd.to_datetime(df['Value Date'])
 
-    st.write(df['Transaction Date'].dtype)
+    st.write(df['Value Date'].dtype)
     
-    #df['Transaction Date'] = df['Transaction Date'].dt.date
-    #df['Transaction Date'] = pd.to_datetime(df['Transaction Date'], format="%Y-%m-%d")
+    #df['Value Date'] = df['Value Date'].dt.date
+    #df['Value Date'] = pd.to_datetime(df['Value Date'], format="%Y-%m-%d")
 
     
     df = df[(df['Withdraw'] < 100000)] 
-    df = df[(df['Deposit'] < 1000000)]
-    df = df.drop(['S.N'], axis=1)
-    df['Day'] = df['Transaction Date'].dt.day_name()
+    df = df[(df['Deposit'] < 100000)]
+    st.markdown("Record size after removing transactions more than 1 lakh: " + str(df.shape))
+    #df = df.drop(['S.N'], axis=1)
+    df['Day'] = df['Value Date'].dt.day_name()
     
     
     st.markdown("TOP 10 DAYS WHERE YOU SPENT THE MOST")
     df_spent = df.sort_values(by='Withdraw', ascending=False).head(10)
     st.write(df_spent)
-    df_spent_index = df_spent.set_index('Transaction Date')['Withdraw']
+    df_spent_index = df_spent.set_index('Value Date')['Withdraw']
 
     st.bar_chart(data=df_spent_index)
     
     st.markdown("TOP 10 DAYS WHERE YOU EARNED THE MOST")
     df_earned =  df.sort_values(by='Deposit', ascending=False).head(10) 
     st.write(df_earned)
-    df_earned_index = df_earned.set_index('Transaction Date')['Deposit']
+    df_earned_index = df_earned.set_index('Value Date')['Deposit']
     st.bar_chart(data=df_earned_index)
     
     st.markdown("MONTHLY EARNING VS SPENDING")
     
-    df['Month'] = df['Transaction Date'].dt.month_name()
-    df['Year'] = df['Transaction Date'].dt.year.astype(str)
+    df['Month'] = df['Value Date'].dt.month_name()
+    df['Year'] = df['Value Date'].dt.year.astype(str)
     
     df['Year-Month'] = df['Year'].astype(str) + '-' + df['Month'].astype(str)
 
